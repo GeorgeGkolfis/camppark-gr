152aa9e23a1df977a880f05569a57d8964cf802e
George Gkolfis <georgiosgolfis@gmail.com>
2024-09-13 17:01:46 +0300

    ...

--- models/campground.js
+++ models/campground.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
     title: String,
-    price: String,
+    image: String,
+    price: Number,
     description: String,
-    location: String
+    location: String,
 });
 
-const Campground = mongoose.model('Campground', CampgroundSchema);
+const Campground = mongoose.model("Campground", CampgroundSchema);
 module.exports = Campground;
