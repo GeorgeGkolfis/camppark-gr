cf7b2703ee0d9588fe22ad35b0f431ff54a41548
George Gkolfis <georgiosgolfis@gmail.com>
2024-09-17 18:03:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const port = 3000;
 const Campground = require("./models/campground");
+const Review = require("./models/review");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 const { campgroundSchema } = require("./schemas.js");
@@ -63,7 +64,6 @@ app.post(
     "/campgrounds",
     validateCampground,
     catchAsync(async (req, res) => {
-        // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
         const campground = new Campground(req.body.campground);
         await campground.save();
         res.redirect(`/campgrounds/${campground._id}`);
@@ -102,6 +102,18 @@ app.delete(
         const { id } = req.params;
         await Campground.findByIdAndDelete(id);
         res.redirect("/campgrounds");
+    })
+);
+
+app.post(
+    "/campgrounds/:id/reviews",
+    catchAsync(async (req, res) => {
+        const campground = await Campground.findById(req.params.id);
+        const review = new Review(req.body.review);
+        campground.reviews.push(review);
+        await review.save();
+        await campground.save();
+        res.redirect(`/campgrounds/${campground._id}`);
     })
 );
 
