54bb8dd0d0bd5d3c4f9be52d791639408cd90b13
George Gkolfis <georgiosgolfis@gmail.com>
2024-09-20 20:10:28 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const express = require("express");
-const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("connect-flash");
 const port = 3000;
-const Campground = require("./models/campground");
-const Review = require("./models/review");
-const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
-const { campgroundSchema, reviewSchema } = require("./schemas.js");
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const campgrounds = require("./routes/campgrounds");
+const reviews = require("./routes/reviews");
 
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/camppark-greece");
@@ -28,16 +25,38 @@ db.on("error", (err) => {
     logError(err);
 });
 
+const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.engine("ejs", ejsMate);
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/campgrounds', campgrounds)
+const sessionConfig = {
+    secret: "secretofthesecrets!",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        // 7 days in milliseconds
+    },
+};
+app.use(session(sessionConfig));
+app.use(flash());
 
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+});
+
+app.use("/campgrounds", campgrounds);
+app.use("/campgrounds/:id/reviews", reviews);
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -57,3 +76,4 @@ app.listen(port, () => {
     const currentTime = new Date().toLocaleString();
     console.log(`SERVING ON PORT ${port}! \nCurrent time: ${currentTime}`);
 });
+ 
