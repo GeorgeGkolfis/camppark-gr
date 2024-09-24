233e3a0149caebb818262d6bfa129332bb31a03c
George Gkolfis <georgiosgolfis@gmail.com>
2024-09-24 15:23:31 +0300

    Update campground.js

--- models/campground.js
+++ models/campground.js
@@ -8,6 +8,10 @@ const CampgroundSchema = new Schema({
     price: Number,
     description: String,
     location: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
     reviews: [
         {
             type: Schema.Types.ObjectId,
@@ -28,5 +32,4 @@ CampgroundSchema.post("findOneAndDelete", async function (doc) {
     }
 });
 
-const Campground = mongoose.model("Campground", CampgroundSchema);
-module.exports = Campground;
+module.exports = mongoose.model("Campground", CampgroundSchema);
