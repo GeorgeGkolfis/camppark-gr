7a88db96d5f42f84f02502398e64d87890a1d527
George Gkolfis <georgiosgolfis@gmail.com>
2024-09-26 16:44:07 +0300

    Update campgrounds.js

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -12,18 +12,14 @@ const campgrounds = require("../controllers/campgrounds");
 router
     .route("/")
     .get(catchAsync(campgrounds.index))
-    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-    .post(upload.array("image", {timeout:60000}), (req,res) =>{
-        console.log(req.body, req.files);
-        res.send("it worked");
-    });
+    .post(isLoggedIn, upload.array("image"), validateCampground, catchAsync(campgrounds.createCampground));
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
 router
     .route("/:id")
     .get(catchAsync(campgrounds.showCampground))
-    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .put(isLoggedIn, isAuthor, upload.array("image"), validateCampground, catchAsync(campgrounds.updateCampground))
     .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
