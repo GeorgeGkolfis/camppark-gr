ff65c212e9cf75688745d285df10484cd719a9b5
George Gkolfis <georgiosgolfis@gmail.com>
2024-09-26 16:42:06 +0300

    Update index.js

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -7,6 +7,7 @@ cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_KEY,
     api_secret: process.env.CLOUDINARY_SECRET,
+    timeout: 60000,
 });
 
 const storage = new CloudinaryStorage({
