d899854625ae8656eb81f5f811f29c7321512ea4
George Gkolfis <georgiosgolfis@gmail.com>
2024-09-26 16:13:32 +0300

    Create index.js

---
+++ cloudinary/index.js
@@ -0,0 +1,23 @@
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+//CLOUDINARY_URL=cloudinary://<your_api_key>:<your_api_secret>@dinr9lnhw
+// "or"
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: "CampPark-Greece",
+        allowedFormats: ["jpeg", "png", "jpg"],
+    },
+});
+
+module.exports = {
+    cloudinary,
+    storage,
+};
