03c865038ce8091b0d56c04a93324985e46b0862
George Gkolfis <georgiosgolfis@gmail.com>
2024-10-01 16:13:27 +0300

    Update schemas.js

--- schemas.js
+++ schemas.js
@@ -1,19 +1,42 @@
 const Joi = require("joi");
+const sanitizeHtml = require("sanitize-html");
 
-module.exports.campgroundSchema = Joi.object({
-    campground: Joi.object({
-        title: Joi.string().required(),
-        price: Joi.number().required().min(0),
-        // image: Joi.string().required(),
-        location: Joi.string().required(),
-        description: Joi.string().required(),
-    }).required(),
-    deleteImages: Joi.array(),
+// Define a custom extension for Joi to add a new rule: escapeHTML
+const extension = (joi) => ({
+    type: "string",
+    base: joi.string(),
+    messages: {
+        "string.escapeHTML": "{{#label}} must not include HTML!",
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [], // No HTML tags allowed
+                    allowedAttributes: {}, // No attributes allowed
+                });
+                if (clean !== value) return helpers.error("string.escapeHTML", { value });
+                return clean;
+            },
+        },
+    },
 });
 
-module.exports.reviewSchema = Joi.object({
-    review: Joi.object({
-        rating: Joi.number().required().min(0).max(5),
-        body: Joi.string().required(),
+const ExtendedJoi = Joi.extend(extension);
+
+module.exports.campgroundSchema = ExtendedJoi.object({
+    campground: ExtendedJoi.object({
+        title: ExtendedJoi.string().required().escapeHTML(),
+        price: ExtendedJoi.number().required().min(0),
+        location: ExtendedJoi.string().required().escapeHTML(),
+        description: ExtendedJoi.string().required().escapeHTML(),
+    }).required(),
+    deleteImages: ExtendedJoi.array(),
+});
+
+module.exports.reviewSchema = ExtendedJoi.object({
+    review: ExtendedJoi.object({
+        rating: ExtendedJoi.number().required().min(0).max(5),
+        body: ExtendedJoi.string().required().escapeHTML(),
     }).required(),
 });
