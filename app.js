84cb52e8176a34f3dff7bcb28c3f5bf6895cca32
George Gkolfis <georgiosgolfis@gmail.com>
2024-10-01 16:13:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-if (process.env.NODE_ENV !== "production") {
+const inProduction = process.env.NODE_ENV === "production";
+
+if (!inProduction) {
     require("dotenv").config();
 }
 
@@ -15,15 +17,21 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const port = 3000;
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const MongoStore = require("connect-mongo");
+
 const ExpressError = require("./utils/ExpressError");
 const User = require("./models/user");
 const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 
+// const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/camppark-greece";
+const dbUrl = "mongodb://127.0.0.1:27017/camppark-greece";
+
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/camppark-greece");
+    await mongoose.connect(dbUrl);
     console.log("INITIAL DB CONNECTION OPEN.");
 }
 
@@ -46,24 +54,83 @@ app.engine("ejs", ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize({ replaceWith: "_" }));
 
 app.use((req, res, next) => {
     req.setTimeout(60000); //server's 60 seconds timeout
     next();
-  });
+});
+
+const mongoSecret = process.env.MONGO_STORE_SECRET || "thisshouldbeasecret!";
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 60 * 60 * 24, //24 hours in seconds
+    crypto: {
+        secret: mongoSecret,
+    },
+});
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e);
+});
+
+const sessionSecret = process.env.SESSION_SECRET || "thisshouldbeasecret!";
 
 const sessionConfig = {
-    secret: "secretofthesecrets!",
+    store,
+    name: "session",
+    secret: sessionSecret,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: inProduction ? false : true, // Avoid unnecessary sessions in production
     cookie: {
         httpOnly: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        secure: inProduction ? true : false, // only accessible via HTTP(S)
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7, // 7 days in milliseconds
         maxAge: 1000 * 60 * 60 * 24 * 7,
-        // 7 days in milliseconds
     },
 };
 app.use(session(sessionConfig));
+app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdn.maptiler.com/",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdn.maptiler.com/",
+];
+const connectSrcUrls = ["https://api.maptiler.com/"];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dinr9lnhw/",
+                "https://images.unsplash.com/",
+                "https://api.maptiler.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -73,10 +140,9 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(flash());
-
 app.use((req, res, next) => {
     // console.log(req.session);
+    res.locals.inProduction = inProduction;
     res.locals.currentUser = req.user; /** from session because of passport */
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
@@ -101,6 +167,7 @@ app.use((err, req, res, next) => {
     res.render("error", { err });
 });
 
+const port = process.env.PORT || 3000;;
 app.listen(port, () => {
     const currentTime = new Date().toLocaleString();
     console.log(`SERVING ON PORT ${port}! \nCurrent time: ${currentTime}`);
