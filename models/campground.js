9991273210ce147a3c4734384d662cbd355ef307
George Gkolfis <georgiosgolfis@gmail.com>
2024-11-20 17:04:05 +0200

    Update campground.js

--- models/campground.js
+++ models/campground.js
@@ -10,8 +10,6 @@ const ImageSchema = new Schema({
 
 ImageSchema.virtual("thumbnail").get(function () {
     return this.url.replace("/upload", "/upload/w_200");
-    // src="https://res.cloudinary.com/dinr9lnhw/image/upload/v1727287386/picked_olives_0_aak0dl.jpg"
-    //replace-regexp
 });
 
 const opts = { toJSON: { virtuals: true } };
@@ -52,13 +50,9 @@ const CampgroundSchema = new Schema(
 CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
     return `<strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
     <p>${this.location}</p>`;
-    // this. refers to the particular campground instance
-    // <p>${this.description.substring(0, 20)}...</p>
 });
 
 CampgroundSchema.post("findOneAndDelete", async function (doc) {
-    // I use "findOneAndDelete" middleware because in app.delete("/campgrounds/:id",.....),
-    // i use (model).findByIdAndDelete() which triggers this specific Query middleware.
     if (doc) {
         await Review.deleteMany({
             _id: {
