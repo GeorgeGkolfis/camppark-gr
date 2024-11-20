07d1b9c430f916372879887ab289bbe6762c5e8c
George Gkolfis <georgiosgolfis@gmail.com>
2024-11-20 17:04:33 +0200

    Update index.js

--- seeds/index.js
+++ seeds/index.js
@@ -22,7 +22,6 @@ db.on("error", (err) => {
 });
 
 const server = app.listen(port, () => {
-    /*...app.listen() starts the server as soon as it's invoked...*/
     const currentTime = new Date().toLocaleString();
     console.log(`SERVING ON PORT ${port}! \nCurrent time: ${currentTime}`);
 });
@@ -30,11 +29,11 @@ const server = app.listen(port, () => {
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 function extractCoordinatesFromUrl(url) {
-    const matches = url.match(/q=(-?\d+.\d+),(-?\d+.\d+)/); // Extract coordinates from Google Maps URL
+    const matches = url.match(/q=(-?\d+.\d+),(-?\d+.\d+)/); 
     if (matches) {
-        return [parseFloat(matches[2]), parseFloat(matches[1])]; // Longitude first
+        return [parseFloat(matches[2]), parseFloat(matches[1])]; 
     }
-    return [0, 0]; // Default in case of no match
+    return [0, 0];
 }
 
 const seedDB = async () => {
@@ -66,42 +65,7 @@ const seedDB = async () => {
                     url: "https://res.cloudinary.com/dinr9lnhw/image/upload/v1727287392/4_the_old_stone_bridge_constructed_in_1787_close_to_tsangarada_village_pelion_mountain-1_oaqcu6.jpg",
                     filename: "CampPark-Greece/4_the_old_stone_bridge_constructed_in_1787_close_to_tsangarada_village_pelion_mountain-1_oaqcu6",
                 },
-                // {
-                //     url: "https://res.cloudinary.com/dinr9lnhw/image/upload/v1727287392/sunset_on_a_traditional_alley_in_megalo_papingo_village_in_ioannina-1_udta5q.jpg",
-                //     filename: "CampPark-Greece/sunset_on_a_traditional_alley_in_megalo_papingo_village_in_ioannina-1_udta5q",
-                // },
-                // {
-                //     url: "https://res.cloudinary.com/dinr9lnhw/image/upload/v1727287392/shutterstock_1188663136_aerial_view_of_the_old_stone_bridge_in_klidonia_zagoria_in_the_autumn_epirus_western_greece._this_arch_bridge_with_elongated_arch_built_in_1853_qnt3j1.jpg",
-                //     filename: "CampPark-Greece/shutterstock_1188663136_aerial_view_of_the_old_stone_bridge_in_klidonia_zagoria_in_the_autumn_epirus_western_greece._this_arch_bridge_with_elongated_arch_built_in_1853_qnt3j1",
-                // },
-                // {
-                //     url: "https://res.cloudinary.com/dinr9lnhw/image/upload/v1727287386/picked_olives_0_aak0dl.jpg",
-                //     filename: "CampPark-Greece/picked_olives_0_aak0dl",
-                // },
-                // {
-                //     url: "https://res.cloudinary.com/dinr9lnhw/image/upload/v1727287386/christian_orthodox_church_in_porto_lagos_vistonida_lake_xanthi-1_hp3klh.jpg",
-                //     filename: "CampPark-Greece/christian_orthodox_church_in_porto_lagos_vistonida_lake_xanthi-1_hp3klh",
-                // },
-                // {
-                //     url: "https://res.cloudinary.com/dinr9lnhw/image/upload/v1727287386/shutterstock_1188660865_aerial_view_of_the_the_vikos_gorge_in_the_autumn_dtxrlk.jpg",
-                //     filename: "CampPark-Greece/shutterstock_1188660865_aerial_view_of_the_the_vikos_gorge_in_the_autumn_dtxrlk",
-                // },
-                // {
-                //     url: "https://res.cloudinary.com/dinr9lnhw/image/upload/v1727287385/3_the_prespa_lakes_also_known_as_prespes_is_probably_one_of_greeces_best-kept_secrets-petar_petkovski-1_yltthf.jpg",
-                //     filename: "CampPark-Greece/3_the_prespa_lakes_also_known_as_prespes_is_probably_one_of_greeces_best-kept_secrets-petar_petkovski-1_yltthf",
-                // },
-                // {
-                //     url: "https://res.cloudinary.com/dinr9lnhw/image/upload/v1727287383/the_magical_landscape_of_mt_boeblx.jpg",
-                //     filename: "CampPark-Greece/the_magical_landscape_of_mt_boeblx",
-                // },
-                // {
-                //     url: "https://res.cloudinary.com/dinr9lnhw/image/upload/v1727287380/4_the_serpentine_nestos_river-1_r7zqds.jpg",
-                //     filename: "CampPark-Greece/4_the_serpentine_nestos_river-1_r7zqds",
-                // },
-                // {
-                //     url: "https://res.cloudinary.com/dinr9lnhw/image/upload/v1727287380/thyme__cheese__bread__tomatoes_and_olives_from_crete_fbaelo.jpg",
-                //     filename: "CampPark-Greece/thyme__cheese__bread__tomatoes_and_olives_from_crete_fbaelo",
-                // },
+                
             ],
         });
         await camp.save();
@@ -115,10 +79,8 @@ seedDB().then(async () => {
         .then(async () => {
             await console.log("DB CONNECTION CLOSED.");
             server.close();
-            // process.exit(0); /*CODE(0):OK...Necessary to exit Node.js, if for some reason doesn't exits!!! */
         })
         .catch((err) => {
             console.error("Error closing database connection!!! : ", err);
-            // process.exit(1); /*CODE(1):ERROR...Necessary to exit Node.js, if for some reason doesn't exits !!! */
         });
 });
