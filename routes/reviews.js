5359ed85f84ca70662d58c45c0ad0f486fbb0926
George Gkolfis <georgiosgolfis@gmail.com>
2024-11-20 17:04:25 +0200

    Update reviews.js

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-// With mergeParams: true, this will include { id: '.......' } in app.use("/campgrounds/:id/reviews", reviews);
 const { isLoggedIn, isReviewAuthor, validateStarRating, validateReview } = require("../middleware");
 const catchAsync = require("../utils/catchAsync");
 const reviews = require("../controllers/reviews");
