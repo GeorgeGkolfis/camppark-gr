7c90828250ee7e6f6e204198f3ccb0b226519096
George Gkolfis <georgiosgolfis@gmail.com>
2024-11-20 17:04:01 +0200

    Update index.js

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,8 +1,6 @@
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-//CLOUDINARY_URL=cloudinary://<your_api_key>:<your_api_secret>@dinr9lnhw
-// "or"
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_KEY,
