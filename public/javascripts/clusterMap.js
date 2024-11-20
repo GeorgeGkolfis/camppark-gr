93eb1606ea75389d99edaafdc89c6073844f3bfa
George Gkolfis <georgiosgolfis@gmail.com>
2024-11-20 17:04:12 +0200

    Update clusterMap.js

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -5,7 +5,6 @@ const map = new maptilersdk.Map({
     style: maptilersdk.MapStyle.BRIGHT,
     center: [23.734832368791103, 37.97556502052681],
     zoom: 5,
-    // navigationControl: false, //disable the navigation control
 });
 
 map.on("load", function () {
@@ -13,8 +12,8 @@ map.on("load", function () {
         type: "geojson",
         data: campgrounds,
         cluster: true,
-        clusterMaxZoom: 14, // Max zoom to cluster points on
-        clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
+        clusterMaxZoom: 14, 
+        clusterRadius: 50, 
     });
 
     map.addLayer({
@@ -23,8 +22,6 @@ map.on("load", function () {
         source: "campgrounds",
         filter: ["has", "point_count"],
         paint: {
-            // Use step expressions (https://docs.maptiler.com/gl-style-specification/expressions/#step)
-            // with three steps to implement three types of circles:
             "circle-color": ["step", ["get", "point_count"], "#93C5FD", 5, "#3B82F6", 10, "#1D4ED8"],
             "circle-radius": ["step", ["get", "point_count"], 15, 5, 20, 10, 25],
         },
@@ -55,7 +52,6 @@ map.on("load", function () {
         },
     });
 
-    // inspect a cluster on click
     map.on("click", "clusters", async (e) => {
         const features = map.queryRenderedFeatures(e.point, {
             layers: ["clusters"],
@@ -68,42 +64,15 @@ map.on("load", function () {
         });
     });
 
-    // When a click event occurs on a feature in
-    // the unclustered-point layer, open a popup at
-    // the location of the feature, with
-    // description HTML from its properties.
     map.on("click", "unclustered-point", function (e) {
         const { popUpMarkup } = e.features[0].properties;
         const coordinates = e.features[0].geometry.coordinates.slice();
 
-        // const campgroundByCoordinates = function (coordinates, tolerance = 0.001) {
-        //     const coordinatesAreClose = function (arr1, arr2, tolerance) {
-        //         if (arr1.length !== arr2.length) return false;
-        //         for (let i = 0; i < arr1.length; i++) {
-        //             if (Math.abs(arr1[i] - arr2[i]) > tolerance) {
-        //                 return false; // Return false if the difference exceeds the tolerance
-        //             }
-        //         }
-        //         return true;
-        //     };
-        //     for (let camp of campgrounds.features) {
-        //         if (coordinatesAreClose(camp.geometry.coordinates, coordinates, tolerance)) {
-        //             return camp;
-        //         }
-        //     }
-        // };
-
-        // const camp = campgroundByCoordinates(coordinates);
-
-        // Ensure that if the map is zoomed out such that
-        // multiple copies of the feature are visible, the
-        // popup appears over the copy being pointed to.
         while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
             coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
         }
 
         new maptilersdk.Popup().setLngLat(coordinates).setHTML(popUpMarkup).addTo(map);
-        //.setHTML(`<h3>${camp.title}</h3><p>${camp.location}</p>`)
     });
 
     map.on("mouseenter", "clusters", () => {
